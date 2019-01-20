b507b9f9619fa6d33d59acf25072612e953ed17f
elijah <43258530+DrProgrammedChild@users.noreply.github.com>
2019-01-19 18:01:38 -0800

    Delete run.bat

--- run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set prefix=?
-set token=NTE4Mjg5NzU4MDcwMjQzMzUz.Du5RAQ.7K_hdsxE8QDFHkv5XQxsIm5v4Hk
-set db_key=pBCQR2nFdEAAAAAAAAAANAHVcf2s5hMBRTRbSpAKhC5J_ueEXlW4qS8dIq1MLjEx
-set ytapikey=AIzaSyAvMpcdeqHI6rCcaj2NWh4DUw3MQb8QsDY
-node index
